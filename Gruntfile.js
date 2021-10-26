8fd813a4fbef977486ba53649c7958584df33e16
Yurii Lukashchuk <urijlukas@gmail.com>
2021-10-26 23:49:27 +0300

    made some changes with service

--- Gruntfile.js
+++ Gruntfile.js
@@ -82,6 +82,7 @@ module.exports = function(grunt) {
         dest: './app/assets/app.css',
         src: [
           'app/styles/app.css',
+          'app/scripts/components/calculation/calc.css'
           //place your Stylesheet files here
         ]
       },
@@ -96,6 +97,8 @@ module.exports = function(grunt) {
           'bower_components/angular-animate/angular-animate.js',
           'app/scripts/homePages.js',
           'app/scripts/components/calculation/calculation.js',
+          'app/scripts/components/calculation/services/common.constant.js',
+          'app/scripts/components/calculation/services/common.service.js',
           'app/scripts/app.js',
           //place your JavaScript files here
         ]
@@ -107,7 +110,7 @@ module.exports = function(grunt) {
         livereload: 7777
       },
       assets: {
-        files: ['app/styles/**/*.css','app/scripts/**/*.js'],
+        files: ['app/styles/**/*.css','app/scripts/**/*.js', 'app/scripts/**/*.html', 'app/scripts/**/*.css'],
         tasks: ['concat']
       },
       protractor: {
--- app/scripts/components/calculation/calculation.js
+++ app/scripts/components/calculation/calculation.js
@@ -1,35 +1,5 @@
 angular.module('app.Calculate', [])
-  .controller('CalculationController', function($scope, welcomeMessage) {
-    var makeArrayMap = function(inputArray) {
-        var newArray = inputArray.filter(
-            e => e.isValid).map(e => {
-            e.name = `Super ${e.name}. Yeah!!`;
-        });
-        console.log(newArray);
-        return newArray;
-    };
-    $scope.initalArray = [
-        {
-            isValid: false,
-            name: 'Ford',
-            order: 0,
-            color: 'red'
-        },
-        {
-            isValid: true,
-            name: 'Mitsubishi',
-            order: 1,
-            color: 'green'
-        },
-        {
-            isValid: true,
-            name: 'Volvo',
-            order: 2,
-            color: 'blue'
-        }
-    ];
-    console.log('Init');
-    console.log($scope.initalArray);
-    console.log('Mapped');
-    $scope.mappedArray = makeArrayMap($scope.initalArray);
-  });
+    .controller('CalculationController', function ($scope, helperService, commonConst) {
+        $scope.initalArray = commonConst.initArray;
+        $scope.mappedArray = helperService.remapInitArray($scope.initalArray);
+    });
