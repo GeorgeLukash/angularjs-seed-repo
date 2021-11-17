19c6ff4260fdc10768ff3e5b7955f2d3fdcdd4dd
vladislav-pro <rossomaha007@gmail.com>
2021-11-17 17:48:15 +0200

    fixed problem wight css, add routing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,8 +3,18 @@ angular.module('myApp', ['ngRoute', 'app.homePages'])
   .constant('TPL_PATH', '/templates')
 
   .config(function($routeProvider, TPL_PATH) {
-    $routeProvider.when('/',{
+    $routeProvider
+    .when('/', {
+      templateUrl: TPL_PATH + '/homePage.template.html'
+    })
+    .when('/login', {
+      templateUrl: TPL_PATH + '/loginArtur.html'
+    })
+    .when('/home', {
+      templateUrl: TPL_PATH + '/homeArtur.html'
+    })
+    .when('/vlad-home',{
       controller : 'HomeCtrl',
-      templateUrl : TPL_PATH + '/home.html'
-    });
+      templateUrl : TPL_PATH + '/vlad-home.html'
   });
+});
